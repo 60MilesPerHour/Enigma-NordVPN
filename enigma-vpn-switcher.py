2af3bcd6f9f625ea938012d024699a3af451f7c7
Miles Oldenburger <32121785+60MilesPerHour@users.noreply.github.com>
2023-02-22 15:38:08 -0800

    Update enigma-vpn-switcher.py

--- enigma-vpn-switcher.py
+++ enigma-vpn-switcher.py
@@ -1,58 +1,39 @@
 import os
 import random
+import time
+import pycountry
+from termcolor import colored
 
-countries = {
-  'ALBANIA': 'AL',
-  'ARGENTINA': 'AR',
-  'AUSTRALIA': 'AU',
-  'AUSTRIA': 'AT',
-  'BELGIUM': 'BE',
-  'BOSNIA AND HERZEGOVINA': 'BA',
-  'BRAZIL': 'BR',
-  'BULGARIA': 'BG',
-  'CANADA': 'CA',
-  'CHILE': 'CL',
-  'COSTA RICA': 'CR',
-  'CROATIA': 'HR',
-  'CYPRUS': 'CY',
-  'CZECH REPUBLIC': 'CZ',
-  'DENMARK': 'DK',
-  'ESTONIA': 'EE',
-  'FINLAND': 'FI',
-  'FRANCE': 'FR',
-  'GEORGIA': 'GE',
-  'GERMANY': 'DE',
-  'GREECE': 'GR',
-  'HUNGARY': 'HU',
-  'ICELAND': 'IS',
-  'INDIA': 'IN',
-  'INDONESIA': 'ID',
-  'IRELAND': 'IE',
-  'ISRAEL': 'IL',
-  'ITALY': 'IT',
-  'JAPAN': 'JP',
-  'LATVIA': 'LV',
-  'LUXEMBOURG': 'LU',
-  'MALAYSIA': 'MY',
-  'MEXICO': 'MX',
-  'MOLDOVA': 'MD',
-  'NETHERLANDS': 'NL',
-  'NEW ZEALAND': 'NZ',
-  'NORWAY': 'NO',
-  'POLAND': 'PL',
-  'PORTUGAL': 'PT',
-  'ROMANIA': 'RO',
-  'SINGAPORE': 'SG',
-  'SLOVAKIA': 'SK',
-  'SLOVENIA': 'SI',
-  'SOUTH AFRICA': 'ZA',
-  'SPAIN': 'ES',
-  'SWEDEN': 'SE',
-  'SWITZERLAND': 'CH',
-  'TURKEY': 'TR',
-  'UNITED KINGDOM': 'GB',
-  'UNITED STATES': 'US',
-}
+supported_vpn_services = ['nordvpn', 'expressvpn']
 
-random_country = random.choice(list(countries.values()))
-os.system('nordvpn c ' + random_country)
+# get list of country codes
+country_codes = [country.alpha_2 for country in pycountry.countries]
+
+# function to connect to a VPN server
+def connect_to_vpn(vpn_service, country_code):
+    if vpn_service not in supported_vpn_services:
+        print(colored(f"{vpn_service} is not a supported VPN service.", 'red'))
+        return False
+    
+    if not country_code:
+        country_code = random.choice(country_codes)
+    elif country_code not in country_codes:
+        print(colored(f"{country_code} is not a valid country code. Trying again with a different one...", 'yellow'))
+        country_code = random.choice(country_codes)
+    
+    os.system(f'{vpn_service} c {country_code}')
+    return True
+
+# main function
+def main(interval):
+    while True:
+        success = connect_to_vpn('nordvpn', None)  # change this to 'expressvpn' if needed
+        if success:
+            print(colored(f"VPN connected to {pycountry.countries.get(alpha_2=os.environ['NORDVPN_COUNTRY']).name}.", 'green'))
+        else:
+            print(colored("Failed to connect to VPN. Retrying...", 'red'))
+        time.sleep(interval * 60)  # convert interval to seconds
+
+if __name__ == '__main__':
+    interval = int(input("Enter the interval (in minutes) for changing VPN: "))
+    main(interval)
